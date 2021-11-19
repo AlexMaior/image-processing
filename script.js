1da42d72d6113aa8712302d7c2823534432cb67a
Alex Maior <alexmaior@yahoo.com>
2021-11-19 21:10:25 +0200

    Added reset function

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-let btn = document.getElementById("tryIt");
+//Reset Image
+let btnReset = document.getElementById("reset");
+btnReset.addEventListener("click", reset);
 
+function reset() {
+  let mat = cv.imread(imgElement);
+  cv.imshow("canvasOutput", mat);
+  mat.delete();
+}
+
+//Convolution
+
+let btn = document.getElementById("convolution");
 btn.addEventListener("click", faCeva);
 
 function faCeva() {
   console.log("CUCU BAU");
-  let src = cv.imread("canvasOutput");
+  //Am observat ca daca in loc de imageScr bagam imaginea din canvas
+  //si dam click pe butonul "Try It" efectul se tot aplica
+  //pana cand imaginea devine "arsa"
+  let src = cv.imread("imageSrc");
   let dst = new cv.Mat();
   let M = cv.Mat.eye(2, 2, cv.CV_32FC1);
   let anchor = new cv.Point(-1, -1);
